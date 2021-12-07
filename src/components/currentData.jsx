60903eebaf8046916ffe6cb845fabcca4a2470b3
djamper103 <xodorenko95@gmail.com>
2021-12-07 11:45:41 +0200

    input commit

--- src/components/currentData.jsx
+++ src/components/currentData.jsx
@@ -41,7 +41,7 @@ export default function Currentdata() {
         }    
     },[])
 
-    useEffect(() => {
+    useEffect(() => {   
         if(cityNameAll){
             let cityDataAll=[]
             cityNameAll.forEach(el=>{
